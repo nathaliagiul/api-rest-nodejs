74a16bd26a49751d0ab888dfe939167b120ae3e1
Nathalia Alves <nathalia.giulliane@outlook.com>
2025-03-05 10:12:46 -0300

    fix: port error

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import {app} from './app'
-import {env} from './env'
+import { app } from './app';
+
+const port = process.env.PORT || 3333;
+const host = ("RENDER" in process.env) ? '0.0.0.0' : 'localhost';
+
+if (!port) {
+  throw new Error("A variável de ambiente PORT não está definida!");
+}
 
 app
   .listen({
-    port: env.PORT,
+    port: Number(port),
+    host: host,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
-  })
+    console.log(`HTTP Server Running on http://${host}:${port}`);
+  });
